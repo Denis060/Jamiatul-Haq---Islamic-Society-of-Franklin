6036aecd94155317d39198b816e65af0affa5475
Ibrahim Denis Fofanah <dfibrahim18@njala.edu.sl.com>
2026-02-18 17:00:45 -0500

    chore: remove debug logs

--- pages/PublicHome.tsx
+++ pages/PublicHome.tsx
@@ -81,7 +81,6 @@ const PublicHome = () => {
 
       for (const p of prayers) {
         let timeStr = prayerTimes[p];
-        console.log(`Raw time for ${p}: '${timeStr}' (Type: ${typeof timeStr})`);
         if (!timeStr) continue;
 
         // Clean up time string (remove extra spaces, handle AM/PM without space, dots)
